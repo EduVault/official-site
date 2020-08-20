5fa62f71f14492600dad17fd113a5495cf1de0b6
Jacob Cohen-Rosenthal <xiaokourou@gmail.com>
2020-08-20 15:09:01 +0800

    let there be code

---
+++ components/home/HomeSectionHero.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <img src="~/assets/img/eduvault-logo.png" alt="EDUVAULT logo" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
